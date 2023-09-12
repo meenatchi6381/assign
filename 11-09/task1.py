77495ebbadfba15d1333ba045b0ae064351c3e0d
meenatchi6381 <144675998+meenatchi6381@users.noreply.github.com>
2023-09-12 11:27:30 +0530

    Add files via upload

---
+++ 11-09/task1.py
@@ -0,0 +1,12 @@
+Name=("Meenatchi Murugesan")
+Address=("MGR nagar naraikinaru - 636118")
+Class=("Msc computer science")
+T=80
+E=80
+M=90
+Phy=90
+Che=80
+Bio=95
+Total=T+E+M+Phy+Che+Bio
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,Bio,"My total mark",Total,"Avg",Avg)
